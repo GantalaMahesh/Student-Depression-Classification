949771cffbaa763ae15e262a3d3ce307ea7963e6
GantalaMahesh <maheshchowhan28.gmail.com>
2025-04-01 17:03:50 +0530

    EDA and Problem Statement

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def get_requirements(file_path:str)->List[str]:
     return requirements
 
 setup(
-    name='Recruitment Management',
+    name='Student Depression Classification',
     version='0.0.1',
     author='Maheshchowhan',
     author_email='maheshchowhan28@gmail.com',
